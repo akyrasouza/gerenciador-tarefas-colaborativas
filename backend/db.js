9d87dc0dad11cd92de96f441886df44bc78c323f
akyrasouza <75281221+akyrasouza@users.noreply.github.com>
2025-05-22 17:52:10 -0300

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,10 +1,9 @@
 import pg from 'pg';
 
-const pool = new pg.Pool({
+export const pool = new pg.Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
     rejectUnauthorized: false,
   },
 });
 
-export default pool;
