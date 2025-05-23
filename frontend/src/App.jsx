6f14cfecdeb4efc663fa0dbcffc912f7f997449b
akyrasouza <75281221+akyrasouza@users.noreply.github.com>
2025-05-23 20:38:52 -0300

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,8 @@ import Login from "./components/Login";
 import Register from "./components/Register";
 import Dashboard from "./components/Dashboard";
 import TaskForm from "./components/TaskForm";
+import AdminArea from './pages/AdminArea';
+import UsuarioArea from './pages/UsuarioArea';
 import "../src/styles/App.css";
 
 function App() {
@@ -10,9 +12,11 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/task/:id?" element={<TaskForm />} />
+        <Route path="/" element={<DashboardLayout />}>
+          <Route path="dashboard" element={<Dashboard />} />
+          <Route path="admin" element={<AdminArea />} />
+          <Route path="usuario" element={<UsuarioArea />} />
+        </Route>
       </Routes>
     </Router>
   );
