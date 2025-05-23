fdacb18e65c95fdad4a5ec4d64bdd76f9423107f
akyrasouza <75281221+akyrasouza@users.noreply.github.com>
2025-05-23 16:03:14 -0300

    Update Dashboard.jsx

--- frontend/src/components/Dashboard.jsx
+++ frontend/src/components/Dashboard.jsx
@@ -1,10 +1,13 @@
 import { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
+import { useAuth } from "../contexts/AuthContext";
+import { Link, useNavigate} from "react-router-dom";
 import api from "../services/api";
 import "../styles/Dashboard.css";
 
 function Dashboard() {
   const [tasks, setTasks] = useState([]);
+ const { logout } = useAuth();
+  const navigate = useNavigate();
 
    useEffect(() => {
     const fetchTasks = async () => {
@@ -18,19 +21,28 @@ function Dashboard() {
     fetchTasks();
   }, []);
 
-   return (
+//Logout do usuário
+const handleLogout = () => {
+    logout();         // Limpa usuário e token
+    navigate("/");    // Redireciona para a tela de login
+  };
+
+ return (
     <div className="dashboard">
-      <h2>Minhas Tarefas</h2>
+      <div className="dashboard-header">
+        <h2>Minhas Tarefas</h2>
+        <button onClick={handleLogout} className="logout-button">Sair</button>
+      </div>
       <Link to="/task" className="new-task-button">Nova Tarefa</Link>
       <div className="card-container">
         {tasks.map((task) => (
           <div key={task.id} className="task-card">
-           <div className="card">
+            <div className="card">
               <h3>{task.title}</h3>
               <p>{task.description}</p>
               <div className="user_status">
-                 <p><strong>Usuário:</strong> {task.creator}</p>
-                  <span className={`status ${task.status}`}>{task.status}</span>
+                <p><strong>Usuário:</strong> {task.creator}</p>
+                <span className={`status ${task.status}`}>{task.status}</span>
               </div>
             </div>
             <Link to={`/task/${task.id}`} className="edit-link">Editar</Link>
