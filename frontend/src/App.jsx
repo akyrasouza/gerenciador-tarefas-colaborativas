ba83624d2639ed5b4673754d8ca2d67ed78a1e13
akyrasouza <75281221+akyrasouza@users.noreply.github.com>
2025-05-23 23:24:27 -0300

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -14,6 +14,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/" element={<DashboardLayout />}>
+          <Route path="/register" element={<Register />} />
           <Route path="dashboard" element={<Dashboard />} />
           <Route path="admin" element={<AdminArea />} />
           <Route path="usuario" element={<UsuarioArea />} />
